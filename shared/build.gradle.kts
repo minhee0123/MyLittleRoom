896fd5526e7a5860102de563b145701eb9342587
minnie <124010199+minheeV@users.noreply.github.com>
2026-03-24 21:09:05 +0900

    fix: 빌드 오류 수정 및 Gradle wrapper 추가
    AGP 버전 호환성 수정(8.5.2), Gradle wrapper(8.11.1) 추가, 런처 아이콘 리소스 생성,
    AndroidManifest Activity 경로 수정 등 빌드 성공에 필요한 설정을 보완

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -11,10 +11,8 @@ plugins {
 kotlin {
     // Android target
     androidTarget {
-        compilations.all {
-            kotlinOptions {
-                jvmTarget = "17"
-            }
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
         }
     }
 
